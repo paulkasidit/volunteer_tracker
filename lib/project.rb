b5e524ea8d711b660735d86dcee534c12e58f4b4
Paul Kasidit <paulkasidit@gmail.com>
2022-10-07 00:39:43 -0700

    wrote test for project volunteer and integration

--- lib/project.rb
+++ lib/project.rb
@@ -1,4 +1,12 @@
 class Project
 
+  attr_reader :id
+  attr_accessor :title
+
+  def initialize(attributes) 
+    @title = attributes.fetch(:word)
+    @id = attributes.fetch(:id)  
+  end
+
 end 
 
