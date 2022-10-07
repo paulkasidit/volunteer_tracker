b34956e48883d44967132253d8d690f59979df61
Paul Kasidit <paulkasidit@gmail.com>
2022-10-07 12:22:10 -0700

    create database for volunteer and project

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/reloader')
 require('./lib/project')
 require('./lib/volunteer')
 require('pry')
+require('pg')
 also_reload('lib/**/*.rb')
 
-
+DB = PG.connect({:dbname => "project_management"})
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,12 @@ require "rspec"
 require "pry"
 require "pg"
 
-# See README. Docker students MUST change line 8 to be compatible with Docker before initially starting their project. Else, they will need to start their project over from scratch.
-# DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+See README. Docker students MUST change line 8 to be compatible with Docker before initially starting their project. Else, they will need to start their project over from scratch.
+DB = PG.connect({:dbname => 'project_management_test'})
 
-# RSpec.configure do |config|
-#   config.after(:each) do
-#     DB.exec('DELETE FROM volunteers *;')
-#     DB.exec('DELETE FROM projects *;')
-#   end
-# end
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
+  end
+end
